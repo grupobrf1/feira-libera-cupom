c0d2f93c9c9bf3f08d6511c532fb9a35d6891de2
Lucas Harã <lucashara20@gmail.com>
2024-07-09 12:10:05 -0300

    chore: Alterar porta do servidor de desenvolvimento para 3001
    - Atualizar porta do servidor de desenvolvimento no vite.config.js de 3000 para 3001.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 3001,
     open: "/login", // Abre automaticamente a página de login
     proxy: {
       "/api": {
